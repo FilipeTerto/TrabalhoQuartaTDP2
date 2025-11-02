5a44916d8cb72b0af34abfa39ea24f2d6b7bbaf5
P3droF <pedro.silva426@fatec.sp.gov.br>
2025-11-02 20:53:54 -0300

    FastAPI, página inicial e MongoDB
    - adicionei gitignore
    - requirements agora tem o venv (ambiente virtual)
    - Conexão com o mongoDB usando o fast api
    - HTML e CSS da página inicial
    - Envio de formulário para o banco de dados

---
+++ Quiz/controller/user_controller.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from repository.quiz_repository import db
+
+router = APIRouter()
+templates = Jinja2Templates(directory="view")
+
+@router.get("/", response_class=HTMLResponse)
+def form_home(request:Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@router.post("/add_user")
+def add_user(name: str = Form(...)):
+    db.users.insert_one({"name": name})
+    return RedirectResponse ("/", status_code=303)
--- Quiz/main.py
+++ Quiz/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from controller.user_controller import router as user_router
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
-@app.get("/")
-def home():
-    return{"mensagem": "Fast está rodando :)"}
+
+app.include_router(user_router)
+
+#monta os estaticos em view 
+app.mount("/static", StaticFiles(directory="view"), name="static")
